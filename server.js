22782242f5357ffd35b3f4c2a38e9d610addea35
himanshuhemu <30832186+himanshuhemu@users.noreply.github.com>
2017-08-09 19:38:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
- res.send ('article two is requested and will be served here');    
+    res.sendFile(path.join(__dirname, 'ui', 'artcle-two.html'));
+   
 });
 
 app.get('/article-three', function (req, res) {
- res.send ('article three is requested and will be served here');    
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));   
 });
 
 
