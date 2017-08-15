10bb5e8a98d92be08f9ad931ac711a9288c4855d
himanshuhemu <30832186+himanshuhemu@users.noreply.github.com>
2017-08-15 22:01:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+//chabge the text   
 var element = document.getElementById("main-text");
 element.innerHTML="new value";
+
+//move the image
+var img = document.getElementById("img")
+img.onclick = function(){
+  img.style.marginleft="100px";  
+};
