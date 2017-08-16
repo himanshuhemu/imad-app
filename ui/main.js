35573dc2d23e408f00779f41188ff5cf258e5f52
himanshuhemu <30832186+himanshuhemu@users.noreply.github.com>
2017-08-16 21:29:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function() {
   
   //capture the list of names and render it as a list.
   
-  var names =["name1","name2","name3","name4"];
+  var names = [ "name1" , "name2" , "name3" , "name4" ];
   var lsit = '';
   for (var i=0 ; i<names.length ; i++){
     list += '<li>'+ names[i] + '</li>';  
